1b7b6f6e6315ebc2be83adcb1b4dacc9732a2f83
Usman Khurshid <usmankq04@gmail.com>
2024-07-21 00:33:23 +0500

    enums added

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,27 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker/models/expense.dart';
 
-class Expenses extends StatefulWidget
-
-{
+class Expenses extends StatefulWidget {
   const Expenses({super.key});
 
   @override
-  State<StatefulWidget> createState()
-  {
+  State<StatefulWidget> createState() {
     return _ExpensesState();
-
   }
-  
 }
 
-
-class _ExpensesState extends State<Expenses>
-{
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Pizza',
+        amount: 1900,
+        date: DateTime.now(),
+        category: Category.food),
+    Expense(
+        title: 'other',
+        amount: 800,
+        date: DateTime.now(),
+        category: Category.other)
+  ];
 
   @override
-  Widget build(context)
-  {
-return Scaffold();
+  Widget build(context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          
+          Center(child: Container(height: 300,width: 300,)),
+        ],
+      ),
+    );
   }
-
 }
--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -2,11 +2,15 @@ import 'package:uuid/uuid.dart';
 
 const uuid=Uuid();
 
-class Expense {
-  Expense({required this.title, required this.amount, required this.date});
+enum Category {food,travel,entertainment,other}
 
+class Expense {
+  Expense({required this.title, required this.amount, required this.date,required this.category}):id=uuid.v4(); // In Dart intializer list can be used to intialize class properties like id with values that are not recieved in the constructor funtion arguemnt.
+
+  
   final String title;
   final double amount;
-  final String id=uuid.v4();
-  final DateTime date;
+  final String id;    //=uuid.v4();
+  final DateTime date; 
+  final Category category;
 }
