d364ed7da992ecbd1c011e15b76e96818e27693b
Usman Khurshid <usmankq04@gmail.com>
2024-07-28 15:27:52 +0500

    .

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/models/expense.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:expense_tracker/expenses_list.dart';
 
 class Expenses extends StatefulWidget {
@@ -28,19 +29,66 @@ class _ExpensesState extends State<Expenses> {
   @override
   Widget build(context) {
     return Scaffold(
-      
       appBar: AppBar(actions: [IconButton(onPressed: (){}, icon: const Icon(Icons.add))],),
-      body: Column(
-        children: [
-                    const Text('The Chart'),
-          
-          Expanded(
-            child: ExpensesList(
-              expenses: _registeredExpenses,
+        body: Column(
+          children: [
+                      const Text('The Chart'),
+
+            Expanded(
+              child: ExpensesList(
+                expenses: _registeredExpenses,
+              ),
             ),
-          ),
-        ],
-      ),
-    );
+          ],
+        ),
+      );
+    }
+       
+       
+    
+    
   }
-}
+
+
+
+
+
+
+
+
+
+//....SLIVER APP BAR...
+   
+       //CustomScrollView(
+    //     slivers: [
+    //       SliverAppBar(
+    //         title: Text('EXPENSE TRACKER',
+    //             style: GoogleFonts.sofia(color: Colors.white, fontSize: 18)),
+    //         actions: [
+    //           IconButton(
+    //             onPressed: () {},
+    //             icon: const Icon(Icons.add),
+    //             alignment: Alignment.topRight,
+    //             color: Colors.white,
+    //             iconSize: 28,
+    //           )
+    //         ],
+    //         expandedHeight: 300,
+    //         backgroundColor: Colors.green,
+    //       ),
+    //       SliverToBoxAdapter(
+    //         child: Padding(
+    //           padding: const EdgeInsets.all(10.0),
+    //           child: Container(
+    //             color: Colors.purple,
+    //             height: 300,
+    //             width: 500,
+    //           ),
+    //         ),
+    //       ),
+       
+
+    //     ExpensesList(expenses: _registeredExpenses)
+    //     ],
+    //   ),
+    // );
